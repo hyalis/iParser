ae4f2510c86604add5d4f73f3e66ff7d6e44678d
oussLaklalech <oussama.laklalech@gmail.com>
2014-03-11 20:30:52 +0100

    ajout des méthodes de la class Question (2)

--- src/main/java/dcll/iParser/iParser/Question.java
+++ src/main/java/dcll/iParser/iParser/Question.java
@@ -12,4 +12,29 @@ public class Question {
 		texte=text;
 		this.type=type;
 	}
+	
+	public void addReponse(Reponse reponse){
+		listReponses.add(reponse);
+	}
+	
+	public boolean resultOf(ArrayList<String> liste){
+		return listReponses.containsAll(liste);
+	}
+	
+	public boolean resultOf(String reponse){
+		return listReponses.contains(reponse);
+	}
+	
+	public int nbReponse(){
+		return listReponses.size();
+	}
+
+	public int nbReponseCorrecte(){
+		int nbrReponseCorrect=0;
+		for(int i=0;i<listReponses.size();i++){
+			if(listReponses.get(i).getReponseValue()==true)
+				nbrReponseCorrect++;
+		}
+		return nbrReponseCorrect;
+	}
 }
