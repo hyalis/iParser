a7aa1d9cb79ba995cb5f533240f184c2684ef6ef
unknown <21003451@208-02.mig.snap.fsi>
2014-03-11 08:35:22 +0100

    #1 Creation du projet Eclipse/Maven

---
+++ src/main/java/dcll/iParser/iParser/App.java
@@ -0,0 +1,13 @@
+package dcll.iParser.iParser;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
