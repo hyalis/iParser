fa50d3c7c939952d36afbef3f63f4f524238247d
akana <mao.akana@gmail.com>
2014-03-16 16:35:34 +0100

    créer classe ParserTest

---
+++ src/test/java/dcll/iParser/iParser/ParserTest.java
@@ -0,0 +1,11 @@
+package dcll.iParser.iParser;
+
+import junit.framework.TestCase;
+
+public class ParserTest extends TestCase{
+	
+	protected void setup() throws Exception{
+		super.setUp();
+	}
+	
+}
