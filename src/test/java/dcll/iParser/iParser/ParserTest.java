04764a798c80901c30969b8542854b7594df772a
akana <mao.akana@gmail.com>
2014-03-24 00:03:08 +0100

    #7 ajouter antonation @Test et ajouter test exception

--- src/test/java/dcll/iParser/iParser/ParserTest.java
+++ src/test/java/dcll/iParser/iParser/ParserTest.java
@@ -1,5 +1,9 @@
 package dcll.iParser.iParser;
 
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
 import junit.framework.TestCase;
 
 public class ParserTest extends TestCase{
@@ -7,6 +11,7 @@ public class ParserTest extends TestCase{
 	private Question questionSimple;
 	private Question questionMultiple;
 	
+	@Test
 	protected void setUp() throws Exception{
 		super.setUp();
 		Parser parser = new Parser();
@@ -28,77 +33,147 @@ public class ParserTest extends TestCase{
 	}
 	
 	//Question Simple
+	@Test
 	public void testQuestionTexteSimple(){
 		assertEquals("Question = Qui est le président des États Unis? ", "Qui est le président des États Unis?", questionSimple.getQuestionText());
 	}
 	
+	@Test
 	public void test1emeReponseTexteSimple(){
 		assertEquals("première réponse = \"Obama.\"","Obama.", questionSimple.getIemeReponse(0).getReponseText());
 	}
 	
+	@Test
 	public void testNature1emeReponseSimple(){
 		assertEquals("Nature de première réponse =  true",true, questionSimple.getIemeReponse(0).getReponseValue());
 	}
 	
+	@Test
 	public void test2emeReponseSimple(){
 		assertEquals("deuxième réponse = \"François Hollande.\"","François Hollande.", questionSimple.getIemeReponse(1).getReponseText());
 	}
 	
+	@Test
 	public void testNature2emeReponseSimple(){
 		assertEquals("Nature de deuxième réponse =  false",false, questionSimple.getIemeReponse(1).getReponseValue());
 	}
 	
+	@Test
 	public void test3emeReponseSimple(){
 		assertEquals("troisième réponse = \"Xi Jinping.\"","Xi Jinping.", questionSimple.getIemeReponse(2).getReponseText());
 	}
 	
+	@Test
 	public void testNature3emeReponseSimple(){
 		assertEquals("Nature de troisième réponse =  false",false, questionSimple.getIemeReponse(2).getReponseValue());
 	}
 	
+	@Test
 	public void testIsQuestionSimple(){
 		assertEquals("Question a une seul réponse",1,questionSimple.nbReponseCorrecte());
 	}
 	
 	//Question Multiple
+	@Test
 	public void testQuestionTexteMultiple(){
 		assertEquals("Question = Quels sont les coleur de feu rouge?","Quelles sont les couleurs des feux tricolores ?", questionMultiple.getQuestionText());
 	}
 	
+	@Test
 	public void test1emeReponseTexteMultiple(){
 		assertEquals("première réponse = \"rouge.\"","rouge.", questionMultiple.getIemeReponse(0).getReponseText());
 	}
 	
+	@Test
 	public void testNature1emeReponseMultiple(){
 		assertEquals("Nature de première réponse =  true",true, questionMultiple.getIemeReponse(0).getReponseValue());
 	}
 	
+	@Test
 	public void test2emeReponseMultiple(){
 		assertEquals("deuxième réponse = \"bleu.\"","bleu.", questionMultiple.getIemeReponse(1).getReponseText());
 	}
 	
+	@Test
 	public void testNature2emeReponseMultiple(){
 		assertEquals("Nature de deuxième réponse =  false",false, questionMultiple.getIemeReponse(1).getReponseValue());
 	}
 	
+	@Test
 	public void test3emeReponseMultiple(){
 		assertEquals("troisième réponse = \"vert.\"","vert.", questionMultiple.getIemeReponse(2).getReponseText());
 	}
 	
+	@Test
 	public void testNature3emeReponseMultiple(){
 		assertEquals("Nature de troisième réponse =  true",true, questionMultiple.getIemeReponse(2).getReponseValue());
 	}
 	
+	@Test
 	public void test4emeReponseMultiple(){
 		assertEquals("troisième réponse = \"noir.\"","noir.", questionMultiple.getIemeReponse(3).getReponseText());
 	}
 	
+	@Test
 	public void testNature4emeReponseMultiple(){
 		assertEquals("Nature de troisième réponse =  false",false, questionMultiple.getIemeReponse(3).getReponseValue());
 	}
 	
+	@Test
 	public void testIsQuestionMultiple(){
 		assertEquals("Question a 2 réponses correctes",2,questionMultiple.nbReponseCorrecte());
 	}
+	
+	@Test (expected = Exception.class)
+	public void testEntree1lignException() throws Exception{
+		String question = "hello";
+		Parser parser = new Parser();
+		parser.doIt(question);
+	}
+	
+	@Test
+	public void testPasSignAvantReponseException() throws Exception{
+		String question = "{Qui est le président des États Unis?\n"+
+				 "|type=\"()\"}\n"+
+				 "+ Obama.\n"+
+				 "François Hollande.\n"+
+				 "- Xi Jinping.\n";
+		Parser parser = new Parser();
+		parser.doIt(question);
+	}
+	
+	@Test(expected = Exception.class)
+	public void testPasEspaceSignReponseException() throws Exception{
+		String question = "{Qui est le président des États Unis?\n"+
+				 "|type=\"()\"}\n"+
+				 "+ Obama.\n"+
+				 "-François Hollande.\n"+
+				 "- Xi Jinping.\n";
+		Parser parser = new Parser();
+		parser.doIt(question);
+	}
+	
+	@Test(expected = Exception.class)
+	public void testPasAccolateException() throws Exception{
+		String question = "Qui est le président des États Unis?\n"+
+				 "|type=\"()\"\n"+
+				 "+ Obama.\n"+
+				 "- François Hollande.\n"+
+				 "- Xi Jinping.\n";
+		Parser parser = new Parser();
+		parser.doIt(question);
+	}
+	
+	@Test(expected = Exception.class)
+	public void testPasTypeException() throws Exception{
+		String question = "{Qui est le président des États Unis?\n"+
+				 "|}\n"+
+				 "+ Obama.\n"+
+				 "- François Hollande.\n"+
+				 "- Xi Jinping.\n";
+		Parser parser = new Parser();
+		parser.doIt(question);
+	}
+	
 	
 }
